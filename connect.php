c66e9f94724b1c7faca8a90a74bb2a2209d792bd
Netesh <48326144+netesh5@users.noreply.github.com>
2020-02-04 21:47:04 +0545

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,22 @@
+<?php
+$name=$_POST['name'];
+$email=$_POST['email'];
+$phone=$_POST['phone'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+
+
+$conn = new mysqli('localhost','root','','contact_details');
+if($conn->connect_error){
+    die('Connection Failed : '.$conn->connect_error);
+}else{
+    $stmt = $conn->prepare("insert into userdata(name,email,phone,subject,message)
+    values(?,?,?,?,?)");
+    $stmt->bind_param("ssiss", $name, $email, $phone, $subject, $message);
+    $stmt->execute();
+    echo"Message Sent";
+    $stmt->close();
+    $conn->close();
+}
+
+?>
